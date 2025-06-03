6128ad1bb94111379db89de6b0a7367fe13f63d9
MetaPiExchange <squadhelpmetapix@gmail.com>
2025-06-03 10:42:48 -0400

    Delete backend/src/handlers/Models

--- backend/src/handlers/Models/user.model.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-  uid: { type: String, required: true, unique: true },
-  username: { type: String, required: true },
-  walletAddress: { type: String },
-}, { timestamps: true });
-
-export const UserModel = mongoose.model('User', userSchema);
