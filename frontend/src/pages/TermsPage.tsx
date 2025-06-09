548add32051f6b018d2f7f0ec2c92f3661fa6583
MetaPiExchange <squadhelpmetapix@gmail.com>
2025-06-09 18:13:52 -0400

    Create TermsPage.tsx

---
+++ frontend/src/pages/TermsPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TermsPage = () => {
+  return (
+    <div className="container">
+      <iframe
+        src="/terms.html"
+        title="Terms of Service"
+        width="100%"
+        height="1000px"
+        style={{ border: 'none' }}
+      />
+    </div>
+  );
+};
+
+export default TermsPage;
